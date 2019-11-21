9798aa498de32c9312b607627fa7abc9b780276a
s-kimyou <s-kimyou@53S500038022.it.bsd405.org>
2019-11-21 15:08:05 -0800

    Checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,10 +4,17 @@
 
 package fracCalc;
 
+import java.util.*;
+
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+    	Scanner userInput = new Scanner(System.in);
+    	System.out.println("Hello! This is your very own Frac Calc.");
+    	System.out.print("Enter an expression to add, subtract, mulitply or divide");
+    	System.out.print(" two integers or fractions");
+    	String eq = userInput.nextLine();
+    	System.out.println(produceAnswer(eq));
         // TODO: Read the input from the user and call produceAnswer with an equation
 
     }
@@ -20,11 +27,15 @@ public class FracCalc {
     //
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
+    public static String produceAnswer(String input) {
+    	Scanner inputUser = new Scanner(input);
+    	String firstVal = inputUser.next();
+    	String pemdas = inputUser.next();
+    	String secondVal = inputUser.next();
+    	
         // TODO: Implement this function to produce the solution to the input
 
-        return "";
+        return secondVal;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
