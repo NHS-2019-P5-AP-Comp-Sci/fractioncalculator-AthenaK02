314fdded49ee3f393cd66b3c2e39277be381446e
s-kimyou <s-kimyou@53S500038022.it.bsd405.org>
2019-11-23 18:08:35 -0800

    Checpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,41 +4,76 @@
 
 package fracCalc;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class FracCalc {
 
     public static void main(String[] args) {
     	Scanner userInput = new Scanner(System.in);
-    	System.out.println("Hello! This is your very own Frac Calc.");
-    	System.out.print("Enter an expression to add, subtract, mulitply or divide");
-    	System.out.print(" two integers or fractions");
-    	String eq = userInput.nextLine();
-    	System.out.println(produceAnswer(eq));
-    	
-        // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	String str = userInput.nextLine();
+    	while (str != "quit") {
+        	System.out.println("Hello! This is your very own Frac Calc.");
+        	System.out.print("Enter an expression to add, subtract, mulitply or divide");
+        	System.out.print(" two integers or fractions.");
+        	System.out.println("Remember to add a space between the operator and each number!");
+        	System.out.println("Type 'quit' to quit.");
+        	String eq = userInput.nextLine();
+        	System.out.println(produceAnswer(eq));
+    	}
+    	if (str == "quit") {
+    		System.out.println("You have quit.");
+    	}
     }
-
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input) {
     	Scanner inputUser = new Scanner(input);
     	String firstVal = inputUser.next();
-    	String pemdas = inputUser.next();
+    	parsing(firstVal);
+    	String mdas = inputUser.next();
     	String secondVal = inputUser.next();
-    	
-        // TODO: Implement this function to produce the solution to the input
+    	String lol = parsing(secondVal);
 
-        return secondVal;
+        return lol;
     }
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
+    public static String parsing(String inp) {
+    	String whole = wholeNum(inp);
+    	String numerator = numNum(inp);
+    	String denominator = denNum(inp);
+    	
+    	String ans = "whole:" + whole + " numerator:" + numerator + " denominator:" + denominator;
+    	return ans;
+    }
+    public static String wholeNum(String one) {
+    	if(one.contains("_")) {
+    		return one.substring(0, one.indexOf("_"));
+    	}
+    	else if(!one.contains("_") && one.contains("/")) {
+    		return "0";
+    	}
+    	else {
+    		return one;
+    	}
+    }
+    public static String numNum(String two) {
+    	if(two.contains("_")) {
+    		return two.substring(two.indexOf("_") + 1, two.indexOf("/"));
+    	}
+    	else if(!two.contains("_") && two.contains("/")) {
+    		return two.substring(0, two.indexOf("/"));
+    	}
+    	else {
+    		return "0";
+    	}
+    }
+    public static String denNum(String three) {
+    	if(three.contains("_")) {
+    		return three.substring(three.indexOf("/") + 1, three.length());
+    	}
+    	else if(!three.contains("_") && three.contains("/")) {
+    		return three.substring(three.indexOf("/") + 1, three.length());
+    	}
+    	else {
+    		return "1";
+    	}
+    }
 
 }
